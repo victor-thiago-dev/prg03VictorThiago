7d7fb3559d5a8265c1a3b74f73c020e5519333e4
victor-thiago-dev <victorsantos.negocios007@gmail.com>
2026-09-11 17:53:12 -0300

    feat: cria classe usuario com os atributos

---
+++ prg03victorthiago/src/main/java/br/com/ifba/usuario/entity/Usuario.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.com.ifba.usuario.entity;
+
+/**
+ *
+ * @author victo
+ */
+public class Usuario {
+    String nome;
+    String cpf;
+    String telefone;
+    String email;
+    String login;
+    String senha;
+    
+}
