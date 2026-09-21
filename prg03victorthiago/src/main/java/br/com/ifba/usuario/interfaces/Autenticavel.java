6729986608612d47bdf35b2aadcfb8791f4b07eb
victor-thiago-dev <victorsantos.negocios007@gmail.com>
2026-09-21 09:17:16 -0300

    Cria classe autenticavel e implementa na classe usuario

---
+++ prg03victorthiago/src/main/java/br/com/ifba/usuario/interfaces/Autenticavel.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.com.ifba.usuario.interfaces;
+
+/**
+ *
+ * @author victo
+ */
+public interface Autenticavel {
+    
+   boolean autenticar(String login, String senha);
+   
+}
