762a3976bde484f5d1061c8eaa18f10f179990d6
victor-thiago-dev <victorsantos.negocios007@gmail.com>
2026-09-23 16:49:04 -0300

    Cria classe perfil

---
+++ prg03victorthiago/src/main/java/br/com/ifba/usuario/entity/Perfil.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.com.ifba.usuario.entity;
+
+/**
+ *
+ * @author victo
+ */
+public class Perfil {
+    private String descricao;
+
+    public Perfil() {
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+    
+}
