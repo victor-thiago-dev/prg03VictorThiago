88c9d8172c080ac5bff88962c3506d17a2a588eb
victor-thiago-dev <victorsantos.negocios007@gmail.com>
2026-09-24 11:14:43 -0300

    Cria classe Pessoa

---
+++ prg03victorthiago/src/main/java/br/com/ifba/pessoa/entity/Pessoa.java
@@ -0,0 +1,57 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.com.ifba.pessoa.entity;
+
+/**
+ *
+ * @author victo
+ */
+public abstract class Pessoa {
+    
+   private String nome;
+   private String cpf;
+   private String telefone;
+   private String email;
+
+    public Pessoa(String nome, String cpf, String telefone, String email) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.telefone = telefone;
+        this.email = email;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+}
