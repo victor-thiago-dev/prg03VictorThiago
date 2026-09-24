58e1b2c8454ea421f79ae269386485d9e1b540c7
victor-thiago-dev <victorsantos.negocios007@gmail.com>
2026-09-24 11:21:19 -0300

    Cria classe Barbeiro com herença de Pessoa

---
+++ prg03victorthiago/src/main/java/br/com/ifba/barbeiro/entity/Barbeiro.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.com.ifba.barbeiro.entity;
+
+import br.com.ifba.pessoa.entity.Pessoa;
+
+/**
+ *
+ * @author victo
+ */
+public class Barbeiro extends Pessoa{
+    
+    private String especialidade;
+
+    public Barbeiro(String nome, String cpf, String telefone, String email) {
+        super(nome, cpf, telefone, email);
+    }
+    
+}
